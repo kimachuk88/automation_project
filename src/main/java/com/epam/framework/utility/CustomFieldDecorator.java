9a246762128e4bd877110b2eb5d3330a34299221
pizhanska <pizhanska29@gmail.com>
2017-03-04 23:25:05 +0200

    edit Custom Field Decorator

--- src/main/java/com/epam/framework/utility/CustomFieldDecorator.java
+++ src/main/java/com/epam/framework/utility/CustomFieldDecorator.java
@@ -4,11 +4,12 @@ import com.epam.framework.controls.base.WebControlImpl;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
-import org.openqa.selenium.support.pagefactory.DefaultFieldDecorator;
-import org.openqa.selenium.support.pagefactory.ElementLocator;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.pagefactory.*;
 
+import java.awt.*;
 import java.lang.reflect.Field;
 
 /**
@@ -18,28 +19,45 @@ public class CustomFieldDecorator extends DefaultFieldDecorator {
 
     protected Logger log = Logger.getLogger("WD");
 
-    public CustomFieldDecorator(final SearchContext searchContext) {
+    public CustomFieldDecorator(SearchContext searchContext) {
         super(new DefaultElementLocatorFactory(searchContext));
     }
 
     @Override
     public Object decorate(ClassLoader loader, Field field) {
-        Class<?> clazz = field.getType();
-        ElementLocator locator = factory.createLocator(field);
-        if (locator == null) {
-            return null;
-        }
-        if (field.getAnnotation(FindBy.class) != null) {
-            if (WebControlImpl.class.isAssignableFrom(field.getType())) {
-                try {
-                    WebElement proxy = proxyForLocator(loader, locator);
-                    return clazz.getConstructor(WebElement.class, String.class,
-                            String.class).newInstance(proxy);
-                } catch (Exception e) {
-                    log.error("WebElement can't be represented as " + clazz);
-                }
+        Class<?> decoratableClass = decoratableClass(field);
+        if (decoratableClass != null) {
+            ElementLocator locator = factory.createLocator(field);
+            if (locator == null) {
+                return null;
             }
+            return createElement(loader, locator, decoratableClass);
         }
         return super.decorate(loader, field);
+    }
+    private Class<?> decoratableClass(Field field) {
+        Class<?> clazz = field.getType();
+        try {
+            clazz.getConstructor(WebElement.class);
+        } catch (Exception e) {
+            return null;
+        }
+        return clazz;
+    }
+
+    protected <T> T createElement(ClassLoader loader,
+                                  ElementLocator locator, Class<T> clazz) {
+        WebElement proxy = proxyForLocator(loader, locator);
+        return createInstance(clazz, proxy);
+    }
+    private <T> T createInstance(Class<T> clazz, WebElement element) {
+        try {
+            return (T) clazz.getConstructor(WebElement.class)
+                    .newInstance(element);
+        } catch (Exception e) {
+            throw new AssertionError(
+                    "WebElement can't be represented as " + clazz
+            );
+        }
     }
 }
