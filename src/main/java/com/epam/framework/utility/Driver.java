bf7903f2f0f67b2b946bd4745500b4384af472dc
pizhanska <pizhanska29@gmail.com>
2017-03-04 23:25:58 +0200

    update Driver class

--- src/main/java/com/epam/framework/utility/Driver.java
+++ src/main/java/com/epam/framework/utility/Driver.java
@@ -1,6 +1,5 @@
 package com.epam.framework.utility;
 
-import com.epam.framework.utility.Config;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,39 +13,39 @@ import java.util.Set;
 public class Driver {
 
     private static Logger log = Logger.getLogger("WD: ");
-    private static WebDriver driver;
+    public static WebDriver instance;
 
     private Driver(){}
 
     public static WebDriver getWebDriverInstance() {
-        if (driver == null) {
+        if (instance == null) {
             switch (Config.getProperty(Config.BROWSER)) {
                 case "chrome":
-                    System.setProperty("webdriver.chrome.driver", WebDriver.class.getResource("/drivers/chromedriver.exe").getPath());
-                    driver = new ChromeDriver();
+                    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Uliana Pizhanska\\Downloads\\cdp\\automation_project\\src\\main\\resources\\drivers\\chromedriver.exe");
+                    instance = new ChromeDriver();
                     log.info("Create instance of Chrome Driver");
                     break;
                 case "firefox":
-                    driver = new FirefoxDriver();
+                    instance = new FirefoxDriver();
                     break;
                 default:
                     throw new IllegalArgumentException("Browser is not supported:" + Config.getProperty(Config.BROWSER));
             }
         }
-        return driver;
+        return instance;
     }
 
-    public void stopDriver() {
-        if (driver != null) {
-            Set<String> windowHandles = driver.getWindowHandles();
+    public static void stopDriver() {
+        if (instance != null) {
+            Set<String> windowHandles = instance.getWindowHandles();
                 for (String handle : windowHandles) {
-                    driver.switchTo().window(handle);
-                    driver.close();
-                    driver.quit();
+                    instance.switchTo().window(handle);
+                    instance.close();
+                    instance.quit();
                 }
                 log.info("Browser is closed");
             }
-            driver = null;
+            instance = null;
         }
 }
 
