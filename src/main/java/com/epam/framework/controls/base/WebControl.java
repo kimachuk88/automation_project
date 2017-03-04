47b432bd7d5d6bf9065fd494a7f6243e09747158
pizhanska <pizhanska29@gmail.com>
2017-03-04 20:02:54 +0200

    create WebControl interface

---
+++ src/main/java/com/epam/framework/controls/base/WebControl.java
@@ -0,0 +1,17 @@
+package com.epam.framework.controls.base;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.Locatable;
+import org.openqa.selenium.internal.WrapsElement;
+
+/**
+ * Created by Uliana Pizhanska on 04/03/2017.
+ */
+public interface WebControl extends WebElement,WrapsElement,Locatable {
+
+    default boolean elementWired(WebElement element) {
+
+        return (element != null);
+
+    }
+}
