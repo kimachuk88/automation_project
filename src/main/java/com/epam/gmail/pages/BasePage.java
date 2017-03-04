cc1f877622adefe63dac900ad91f45c97a67f1da
pizhanska <pizhanska29@gmail.com>
2017-03-04 23:56:48 +0200

    add BasePage

---
+++ src/main/java/com/epam/gmail/pages/BasePage.java
@@ -0,0 +1,27 @@
+package com.epam.gmail.pages;
+import com.epam.framework.utility.CustomFieldDecorator;
+import com.epam.framework.utility.Driver;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Uliana Pizhanska on 27/02/2017.
+ */
+public abstract class BasePage {
+
+    protected Logger log = Logger.getLogger("WD: ");
+
+    public BasePage(){
+        PageFactory.initElements(new CustomFieldDecorator(Driver.instance),this);
+    }
+
+
+
+
+
+
+
+
+
+
+}
