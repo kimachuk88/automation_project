6a7939674ae39e67b9b5adef182f3e6a081896a8
pizhanska <pizhanska29@gmail.com>
2017-03-05 23:30:19 +0200

    add some methods for BasePage

--- src/main/java/com/epam/gmail/pages/BasePage.java
+++ src/main/java/com/epam/gmail/pages/BasePage.java
@@ -2,7 +2,14 @@ package com.epam.gmail.pages;
 import com.epam.framework.utility.CustomFieldDecorator;
 import com.epam.framework.utility.Driver;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.*;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Uliana Pizhanska on 27/02/2017.
@@ -10,8 +17,40 @@ import org.openqa.selenium.support.PageFactory;
 public abstract class BasePage {
 
     protected Logger log = Logger.getLogger("WD: ");
+    protected Wait<WebDriver> wait;
 
     public BasePage() {
         PageFactory.initElements(new CustomFieldDecorator(Driver.instance), this);
     }
+
+    protected boolean waitForControl(WebElement webControl) {
+        log.info("Wait page to be loaded");
+        wait = getWait(60);
+        return wait.until(webDriver -> webControl != null && webControl.isDisplayed());
+    }
+
+    void waitForPageLoad() {
+        new WebDriverWait(Driver.instance, 30).until((ExpectedCondition<Boolean>) wd ->
+                ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
+    }
+
+    private FluentWait getWait(int timeout) {
+        return new FluentWait<>(Driver.instance)
+                .withTimeout(timeout, TimeUnit.SECONDS)
+                .pollingEvery(2, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+    }
+
+    public void waitForAlert(int sec){
+
+        WebDriverWait wait = new WebDriverWait(Driver.instance, sec, 100);
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+    public String getPageTitle(){
+        return Driver.instance.getTitle();
+    }
+
+
 }
+
