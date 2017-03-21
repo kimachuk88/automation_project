a345fcba631b8e28b9fb88eefbee90cd3195c668
pizhanska <pizhanska29@gmail.com>
2017-03-21 23:46:58 +0200

    update DataProvider

--- src/main/java/com/epam/framework/utility/DataProvd.java
+++ src/main/java/com/epam/framework/utility/DataProvd.java
@@ -2,35 +2,35 @@ package com.epam.framework.utility;
 
 import org.testng.annotations.DataProvider;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * Created by Uliana Pizhanska on 05/03/2017.
  */
 public class DataProvd {
 
-    @DataProvider(name="login")
-    public static Object[][] loginTitle(){
+    @DataProvider(name = "login")
+    public static Object[][] loginTitle() {
         return new Object[][]{
                 {"Gmail"}
         };
     }
 
-    @DataProvider(name="createMessage")
-    public static Object[][] createMessage() {
-        Properties cred = new Properties();
-        try {
-            FileInputStream input = new FileInputStream("src/main/resources/input.properties");
-            cred.load(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Object[][] creds = new Object[1][3];
-        creds[0][0] = cred.getProperty("account");
-        creds[0][1] = cred.getProperty("email");
-        creds[0][2] = cred.getProperty("subj");
-        return creds;
+    @DataProvider(name = "createMessage")
+    public static Object[][] createMessage() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/data.csv"));
+        List<String[]> list = bufferedReader.lines()
+                .map(line -> line.split(","))
+                .collect(Collectors.toList());
+        String[][] data = list.toArray(new String[list.size()][]);
+        return data;
     }
 }
