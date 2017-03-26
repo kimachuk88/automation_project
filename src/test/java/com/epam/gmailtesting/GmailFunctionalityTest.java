68f3554006d8b3e2d41f46573c17805034572d4e
pizhanska <pizhanska29@gmail.com>
2017-03-26 23:12:07 +0300

    update POM

--- src/test/java/com/epam/gmailtesting/GmailFunctionalityTest.java
+++ src/test/java/com/epam/gmailtesting/GmailFunctionalityTest.java
@@ -2,6 +2,7 @@ package com.epam.gmailtesting;
 
 import com.epam.framework.utility.Config;
 import com.epam.framework.utility.DataProvd;
+import com.epam.gmail.bo.LoginBO;
 import com.epam.gmail.bo.SendMessageBO;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,41 +13,40 @@ import org.testng.annotations.Test;
 public class GmailFunctionalityTest extends BaseTest {
 
     private SendMessageBO sendMessageBO;
+    private LoginBO loginBO;
 
     public void setup(){
         sendMessageBO = new SendMessageBO();
+        loginBO = new LoginBO();
     }
 
 
     @Test(description = "Login, open custom search page\n",  dataProviderClass = DataProvd.class, dataProvider = "login")
-    public void login(String homeTitle){
+    public void login(String homeTitle, String email,String accountName){
         setup();
-        String title = sendMessageBO.checkPageTitle();
-        Assert.assertEquals(title, homeTitle);
-        sendMessageBO.login(Config.getProperty(Config.USERNAME),Config.getProperty(Config.PASSWORD));
+        Assert.assertEquals(loginBO.checkPageTitle(), homeTitle);
+        loginBO.loginEmail(Config.getProperty(Config.USERNAME));
+        Assert.assertEquals(loginBO.getEmailDisplayed(),email);
+        loginBO.loginPasswd(Config.getProperty(Config.PASSWORD));
+        Assert.assertTrue(sendMessageBO.checkAccountName().contains(accountName));
     }
 
-    @Test( dependsOnMethods = "login", description = "" +
+    @Test( dependsOnMethods = "login", description = "\n" +
             "1. Check account name and click on Compose Message\n" +
             "2. Set receiver, message subject and message body, click on Send\n" +
             "3. Go to Sent folder and check details of sent message\n",
             dataProviderClass = DataProvd.class, dataProvider = "createMessage")
-    public void sendMessage(String accountName, String receiveEmail, String messageText){
-        String title = sendMessageBO.checkAccountName();
-        Assert.assertEquals(title, accountName);
+    public void sendMessage( String receiveEmail, String messageText){
         sendMessageBO.createMessage(receiveEmail,messageText);
         sendMessageBO.sendMessage();
-        title = sendMessageBO.checkSentMessage();
-        Assert.assertEquals(title,messageText);
+        Assert.assertEquals(sendMessageBO.checkSentMessage(),messageText);
     }
 
-    @Test( dependsOnMethods = "login", description = "" +
+    @Test( dependsOnMethods = "login", description = "\n" +
             "1. Check account name and click on Compose Message\n" +
             "2. Set receiver, message subject and message body, click on Send\n" +
             "3. Go to Drafts folder and check if message is saved and delete it", dataProviderClass = DataProvd.class, dataProvider = "createMessage")
-    public void saveAsDraftAndDelete(String accountName, String receiveEmail, String messageText){
-        String title = sendMessageBO.checkAccountName();
-        Assert.assertEquals(title, accountName);
+    public void saveAsDraftAndDelete(String receiveEmail, String messageText){
         sendMessageBO.createMessage(receiveEmail,messageText);
         sendMessageBO.deleteFromDrafts();
     }
